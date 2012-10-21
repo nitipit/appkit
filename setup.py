9481b63f604d073ba903773317dc1843f3046576
Um Nontasuwan <nitipit@gmail.com>
2012-10-22 00:33:37 +0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+import os
+
+data = list()
+for d in os.walk('appkit/'):
+    if len(d[2]) > 0:
+        path_list = map(
+            lambda x: str.join('/', os.path.join(d[0], x).split('/')[1:]),
+            d[2]
+        )
+        data.extend(path_list)
+
+setup(
+    name="AppKit",
+    version="0.1",
+    package_dir={'appkit': 'appkit'},
+    packages=['appkit'],
+    package_data={'appkit': data}
+)
