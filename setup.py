4f4170e5ab2ecb234501383991be3050a7b143ca
Um Nontasuwan <nitipit@gmail.com>
2013-01-04 01:55:53 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,10 +11,17 @@ for d in os.walk('appkit/'):
         data.extend(path_list)
 
 setup(
-    name="AppKit",
-    version="0.1",
+    name='AppKit',
+    version='0.1',
+    description='Desktop application framework based on Webkit, \
+        HTML5, CSS3, Javascript and Python',
+    author='Nitipit Nontasuwan',
+    author_email='nitipit@gmail.com',
+    url='http://nitipit.github.com/appkit/',
+    license='MIT',
+    platforms=['Linux', ],
     package_dir={'appkit': 'appkit'},
     packages=['appkit'],
     package_data={'appkit': data},
-    install_requires=['sphinx_bootstrap_theme', ],
+    requires=['sphinx_bootstrap_theme', ],
 )
