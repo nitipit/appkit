c7d5edffb884dc9a3973f1e089f9395019188555
Um Nontasuwan <nitipit@gmail.com>
2013-02-04 17:06:51 +0700

    Bump to 0.2.3 development version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ for d in os.walk('appkit/'):
 
 setup(
     name='AppKit',
-    version='0.2.2',
+    version='0.2.3',
     description='Desktop application framework based on Webkit, \
         HTML5, CSS3, Javascript and Python',
     author='Nitipit Nontasuwan',
