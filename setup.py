63bd6d1c9844b66c3ce2279fb16899c4f182df44
Um Nontasuwan <nitipit@gmail.com>
2013-03-07 14:10:00 +0700

    Fix package description

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ for d in os.walk('appkit/'):
 setup(
     name='AppKit',
     version='0.2.3',
-    description='Desktop application framework based on Webkit, \
-        HTML5, CSS3, Javascript and Python',
+    description='Desktop application framework based on Webkit' +\
+        ' HTML5, CSS3, Javascript and Python',
     author='Nitipit Nontasuwan',
     author_email='nitipit@gmail.com',
     url='http://nitipit.github.com/appkit/',
